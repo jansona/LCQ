95cc9c318d77f1029d0deae88b966985721b2be6
fansking <935802216@qq.com>
2018-12-21 10:31:02 +0800

    更改getlocalIp

--- PigeonWindows/PigeonWindows/UdpHandler.cs
+++ PigeonWindows/PigeonWindows/UdpHandler.cs
@@ -107,27 +107,50 @@ namespace UDPClient
 
         public static string GetLocalIP()
         {
-            string result = RunApp("route", "print", true);
-            Match m = Regex.Match(result, @"0.0.0.0\s+0.0.0.0\s+(\d+.\d+.\d+.\d+)\s+(\d+.\d+.\d+.\d+)");
-            if (m.Success)
+            try
             {
-                return m.Groups[2].Value;
+
+                string HostName = Dns.GetHostName(); //得到主机名
+                IPHostEntry IpEntry = Dns.GetHostEntry(HostName);
+                for (int i = 0; i < IpEntry.AddressList.Length; i++)
+                {
+                    //从IP地址列表中筛选出IPv4类型的IP地址
+                    //AddressFamily.InterNetwork表示此IP为IPv4,
+                    //AddressFamily.InterNetworkV6表示此地址为IPv6类型
+                    if (IpEntry.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        string ip = "";
+                        ip = IpEntry.AddressList[i].ToString();
+                        return IpEntry.AddressList[i].ToString();
+                    }
+                }
+                return "";
             }
-            else
+            catch (Exception ex)
             {
-                try
-                {
-                    System.Net.Sockets.TcpClient c = new System.Net.Sockets.TcpClient();
-                    c.Connect("www.baidu.com", 80);
-                    string ip = ((System.Net.IPEndPoint)c.Client.LocalEndPoint).Address.ToString();
-                    c.Close();
-                    return ip;
-                }
-                catch (Exception)
-                {
-                    return null;
-                }
+                return ex.Message;
             }
+            //string result = RunApp("route", "print", true);
+            //Match m = Regex.Match(result, @"0.0.0.0\s+0.0.0.0\s+(\d+.\d+.\d+.\d+)\s+(\d+.\d+.\d+.\d+)");
+            //if (m.Success)
+            //{
+            //    return m.Groups[2].Value;
+            //}
+            //else
+            //{
+            //    try
+            //    {
+            //        System.Net.Sockets.TcpClient c = new System.Net.Sockets.TcpClient();
+            //        c.Connect("www.baidu.com", 80);
+            //        string ip = ((System.Net.IPEndPoint)c.Client.LocalEndPoint).Address.ToString();
+            //        c.Close();
+            //        return ip;
+            //    }
+            //    catch (Exception)
+            //    {
+            //        return null;
+            //    }
+            //}
         }
 
         public static string RunApp(string filename, string arguments, bool recordLog)
